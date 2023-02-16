7024c42a8c5cf8353bd41d8216d445aba4284e2a
Flavio Silva <flaviovieira94@gmail.com>
2023-02-15 21:39:29 -0300

    foi realizado uma correção no escript, pois estava fazendo o calculo errado e foi acrecentado mais algumas condiçoes

--- aula12.py
+++ aula12.py
@@ -1,6 +1,11 @@
 name = input('qual é o seu nome: ')
 ano_nascimento = int(input('qual é seu ano nascimento: '))
-idade = ano_nascimento - 2023
+idade = 2023 - ano_nascimento
+
 if idade < 18:
     print('você é menor de idade')
+elif idade > 18 and idade <= 28:
+    print ('você é está com meia idade')
+elif idade > 28 and idade <= 40:
+    print('você já é velho')
     
