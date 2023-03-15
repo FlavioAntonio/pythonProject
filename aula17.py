a253e916becb582ae4757afd52c2913bf34562cf
Flavio Silva <flaviovieira94@gmail.com>
2023-03-14 21:57:07 -0300

    Aula de fatiamento de string

--- aula17.py
+++ aula17.py
@@ -1,10 +1,13 @@
- """
+'''
 >  - esquerda
 < - direita
 ^ - centro
+'''
 
-"""
 
+#
 variavel = 'ABC'
-print(f'{variavel}')
-print(f'{variavel:*^10}')
+# print(f'{variavel}')
+# print(f'{variavel:*^10}')
+
+print(len(variavel))
