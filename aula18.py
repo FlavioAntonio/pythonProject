484c8c348418d4ecff63f381f89599c75cdebcaa
Flavio Silva <flaviovieira94@gmail.com>
2023-03-20 21:07:27 -0300

    adicionando algumas linhas

--- aula18.py
+++ aula18.py
@@ -7,6 +7,7 @@ if nome != '' and idade != '':
     print(f'seu nome é {nome}')
     print(f'sua idade é {idade}')
     print(f' seu nome invertido é {nome[::-1]}')
+    print(f'seu nome contem {len(nome)} letras')
     print(f'seu nome contem eapaços ', " " in nome)
     print(f' a primeira letra do seu nome é {nome[0]}')
     qt = qt -1
