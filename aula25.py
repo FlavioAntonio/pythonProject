eab1fca32c8b3c6ead87a64ecd0affb3b5a6e82e
flavio <flaviovieira94@gmail.com>
2023-04-29 18:19:00 -0300

    tabuada

--- aula25.py
+++ aula25.py
@@ -9,20 +9,15 @@ cont = 1
 while cont <= 10:
 
     if condicao == 1:
-
         resultado = valor + cont
         print(f'{valor} + {cont} = {resultado:.0f}')
     elif condicao == 2:
-
         resultado = valor / cont
-
         print(f'{valor} / {cont} = {resultado:.1f}')
     elif condicao == 3:
-
         resultado = valor * cont
         print(f'{valor} * {cont} = {resultado:.0f}')
     elif condicao == 4:
-
         resultado = valor - cont
         print(f'{cont} - {valor} = {resultado:.0f}')
     else:
