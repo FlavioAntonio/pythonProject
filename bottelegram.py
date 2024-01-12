7dcc741f1837e35b8c9f20e570d9a088ce920fa2
flavio <flaviovieira94@gmail.com>
2024-01-12 13:54:59 -0300

    foi acresentado uma linha para poder enviar arquivos do servidor para o bot

--- bottelegram.py
+++ bottelegram.py
@@ -25,18 +25,17 @@ bot = telebot.TeleBot('6349438511:AAFIaITWjswPa259XF5m8yTsVPkPhxzAmJ8')
 def execute_bate(filename):
     os.system(filename)
     #bot.send_message(message.chat.id, 'Escript executado')
-
+    return filename
 @bot.message_handler(content_types=['text'])
 def ativar_windows(message):
     executar = message.text
     if executar == "1":
-        execute_bate(r"C:\Users\AN20165014\Documents\script\windows.bat")
+        #resultado = execute_bate(r"C:\Users\AN20165014\Documents\script\windows.bat")
+        # alinha abaixo envia o texto do arquivo
+        with open(r"C:\Users\AN20165014\Documents\script\arquivo.txt", 'rb') as arquivo:
+            bot.send_message(message.chat.id, arquivo)
+
     else:
         bot.send_message(message.chat.id,"Valor invalido")
-
-
-
-
-
 # Inicie o bot
 bot.polling()
