2349fba1560430a64ac990d56ba4be8551b94b0a
flavio <flaviovieira94@gmail.com>
2024-01-16 12:44:01 -0300

    programa para inserir itens em uma lista

--- aula28.py
+++ aula28.py
@@ -1,1 +1,36 @@
+import os
 
+lista = []
+opcao = True
+
+while opcao != 's':
+    print('Selecione uma opção')
+    opcao = input('[i]Inserir [a]Apagar [l]Listar [S]Sair')
+
+    if opcao == 'i':
+        os.system('cls')
+        valor = input('Valor: ')
+        lista.append(valor)
+    elif opcao == 'a':
+        indice_str = input('Escolha o indice para apagar: ')
+        #tratando possiveis erros
+        try:
+            indice = int(indice_str)
+            del lista[indice]
+        except ValueError:
+            print('Por favor digite numero inteiro')
+        except IndexError:
+            print('O indice não existe na lista')
+        except Exception:
+            print('Erro desconhecido')
+
+    elif opcao == 'l':
+
+        os.system('cls')
+        if len(lista) == 0:
+            print('Nada para listar')
+        for i, valor in enumerate(lista):
+            print(i, valor)
+
+    else:
+        print('escolha uma i, a ou l')
