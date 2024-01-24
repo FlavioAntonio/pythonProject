4b9b52241eb5aed479c45c384bca59c1b0c1d207
flavio <flaviovieira94@gmail.com>
2024-01-23 22:19:14 -0300

    software concluido

--- aula31.py
+++ aula31.py
@@ -2,27 +2,31 @@ def validar_cpf():
     #capiturando o cpf
     cpf = input('Digite seu cpf')
 
-    lista_digitos = cpf[:9]
+    nove_digitos = cpf[:9]
     contador_1 = 10
     resultado_1 = 0
-    for digito in lista_digitos:
-        resultado_1 += int(digito) * contador_1
+    for digito1 in nove_digitos:
+        resultado_1 += int(digito1) * contador_1
         contador_1 -=1
-        digito = (resultado_1 * 10) % 11
-        digito = digito if digito <= 9 else 0
-    print(digito)
+    digito_1 = (resultado_1 * 10) % 11
+    digito_1 = digito_1 if digito_1 <= 9 else 0
     #segunda parte do codigo
 
-    lista_digitos = cpf[:10]
+    dez_digitos = nove_digitos + str(digito_1)
     contador_2 = 11
-    resultado_2 = 0
-    for digito1 in lista_digitos:
-        resultado_2 += int(digito1) * contador_2
+    resultado_digito_2 = 0
+
+    for digito2 in dez_digitos:
+        resultado_digito_2 += int(digito2) * contador_2
         contador_2 -= 1
-        digito1 = (resultado_2 * 10) % 11
-        #print(digito1)
-        digito1 = digito1 if digito1 <= 9 else 0
-    print(digito1)
+    digito_2 = (resultado_digito_2 * 10) % 11
+    digito_2 = digito_2 if digito_2 <= 9 else 0
+
+    cpf_gerado_pelo_calculo = f'{nove_digitos}{digito_1}{digito_2}'
+    if cpf_gerado_pelo_calculo == cpf:
+        print(f'o cpf: {cpf_gerado_pelo_calculo} é Valido')
+    else:
+        print(f'o cpf: {cpf_gerado_pelo_calculo} Não é Valido')
 
 validar_cpf()
 
