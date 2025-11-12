d5ba640d0c1282994d555a1fe6a892b041bd210b
Antonio Flavio Vieira da Silva <flaviovieira94@gmail.com>
2025-11-11 23:17:22 -0300

    subindo aula sobre log

---
+++ aula141/aula141.py
@@ -0,0 +1,6 @@
+from log import LogFileMinin, LogPrintMixin
+
+teste = "erro ao executar o programa"
+
+lp = LogFileMinin()
+lp.log_error(teste)
---
+++ aula141/log.py
@@ -0,0 +1,36 @@
+from pathlib import Path
+
+LOG_FILE = Path(__file__).parent / 'log.txt'
+
+class Log:
+    def _log(self, msg):
+        raise NotImplementedError('implemente o tetodo log')
+    
+    def log_error(self, msg):
+        return self._log(f'Error: {msg}')
+    
+    def log_success(self, msg):
+        return self._log(f'Success: {msg}')
+    
+class LogFileMinin(Log):
+    def _log(self, msg):
+        msg_formatada = f'{msg} ({self.__class__.__name__})'
+        with open(LOG_FILE, 'a') as arquivo:
+            arquivo.write(msg)
+            arquivo.write('\n')
+        
+
+class LogPrintMixin(Log):
+    def _log(self, msg):
+        print(f'{msg} ({self.__class__.__name__})')
+
+if __name__ == '__main__':
+    lp = LogPrintMixin()
+    lp.log_error('qualquer coisa')
+    lp.log_success('que legal ')
+    print(LOG_FILE)
+
+    lf = LogFileMinin()
+    lf.log_error('qualquer coisa')
+    lf.log_success('sucesso')
+    
