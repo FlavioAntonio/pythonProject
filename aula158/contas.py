e400e11d186b29c3c2d9e29b941c4c7c11f89f77
Antonio Flavio Vieira da Silva <flaviovieira94@gmail.com>
2025-12-25 21:29:59 -0300

    criando a class coontaCorrent e ContaPoupança

--- aula158/contas.py
+++ aula158/contas.py
@@ -1,7 +1,7 @@
 import abc
 
 class Conta(abc.ABC):
-    def __init__(self, agencia, conta, saldo):
+    def __init__(self, agencia, conta, saldo = 0):
         self.agencia = agencia
         self.conta = conta
         self.saldo = saldo
@@ -29,11 +29,38 @@ class ContaPoupanca(Conta):
     
         print("saldo insuficiente para saque")
         self.detalhes(f'SAQUE NEGADO {valor:.2f}')
+
+
+class ContaCorrente(Conta):
+    def __init__(self, agencia, conta, saldo = 0,  limite = 0):
+        super().__init__(agencia, conta, saldo)
+        self.limite = limite
+
+
+    def sacar(self, valor):
+        valor_pos_saque = self.saldo - valor
+        limite_maximo = -self.limite
+
+        if valor_pos_saque >= limite_maximo:
+            self.saldo -= valor
+            self.detalhes(f"saque de {valor:.2f} realizado com sucesso")
+            return self.saldo
+
+        print("saldo insuficiente para saque")
+        print(f'seu limite é {-self.limite:.2f}')
+        self.detalhes(f'SAQUE NEGADO {valor:.2f}')
         
         
 if __name__ == "__main__":
-    cp1 = ContaPoupanca("0001", "12345-6", 1000)
-    cp1.sacar(500)
-    cp1.depositar(200)
+    # cp1 = ContaPoupanca("0001", "12345-6", 1000)
+    # cp1.sacar(500)
+    # cp1.depositar(200)
+    # print("*************************")
+    cc1 = ContaCorrente("0001", "12345-6", 100, 100)
+
+    print(cc1.saldo)
+    cc1.sacar(250)
+
+    #cc1.depositar(200)
     
         
