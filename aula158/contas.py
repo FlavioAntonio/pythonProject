8658bd2f68178a41c030b6c6fe278fb6346971e4
Antonio Flavio Vieira da Silva <flaviovieira94@gmail.com>
2025-12-27 22:04:43 -0300

    Corrigindo a classe conta e pessoas

--- aula158/contas.py
+++ aula158/contas.py
@@ -1,41 +1,48 @@
 import abc
 
+
 class Conta(abc.ABC):
-    def __init__(self, agencia, conta, saldo = 0):
+    def __init__(self, agencia: int, conta: int, saldo: float = 0) -> None:
         self.agencia = agencia
         self.conta = conta
         self.saldo = saldo
-        
+
     @abc.abstractmethod
-    def sacar(self, valor):
+    def sacar(self, valor: float) -> float:
         ...
-        
-    def depositar(self, valor):
+
+    def depositar(self, valor: float) -> float:
         self.saldo += valor
         self.detalhes(f"deposito {valor:.2f} realizado com sucesso")
+        return self.saldo
 
-    def detalhes(self, msg=""):
+    def detalhes(self, msg: str = "") -> None:
         print(f"o seu saldo é {self.saldo:.2f} {msg}")
-        
+
+    def __repr__(self):
+        class_name = type(self).__name__
+        attrs = f'{self.agencia!r}, {self.conta!r}, {self.saldo!r}'
+        return f'{class_name}{attrs}'
+
 
 class ContaPoupanca(Conta):
     def sacar(self, valor):
         valor_pos_saque = self.saldo - valor
-        
+
         if valor_pos_saque >= 0:
             self.saldo -= valor
             self.detalhes(f"saque de {valor:.2f} realizado com sucesso")
             return self.saldo
-    
+
         print("saldo insuficiente para saque")
         self.detalhes(f'SAQUE NEGADO {valor:.2f}')
+        return self.saldo
 
 
 class ContaCorrente(Conta):
-    def __init__(self, agencia, conta, saldo = 0,  limite = 0):
+    def __init__(self, agencia: int, conta: int, saldo: int = 0, limite: int = 0):
         super().__init__(agencia, conta, saldo)
         self.limite = limite
-
 
     def sacar(self, valor):
         valor_pos_saque = self.saldo - valor
@@ -45,22 +52,26 @@ class ContaCorrente(Conta):
             self.saldo -= valor
             self.detalhes(f"saque de {valor:.2f} realizado com sucesso")
             return self.saldo
+    def __repr__(self):
+        class_name = type(self).__name__
+        attrs = (f'{self.agencia!r}, {self.conta!r}, {self.saldo!r},'
+                 f' {self.limite!r}')
+        return f'{class_name}{attrs}'
 
         print("saldo insuficiente para saque")
         print(f'seu limite é {-self.limite:.2f}')
         self.detalhes(f'SAQUE NEGADO {valor:.2f}')
-        
-        
+        return self.saldo
+
+
 if __name__ == "__main__":
     # cp1 = ContaPoupanca("0001", "12345-6", 1000)
     # cp1.sacar(500)
     # cp1.depositar(200)
     # print("*************************")
-    cc1 = ContaCorrente("0001", "12345-6", 100, 100)
+    cc1 = ContaCorrente(100, 123456, 100, 100)
 
     print(cc1.saldo)
     cc1.sacar(250)
 
-    #cc1.depositar(200)
-    
-        
+    # cc1.depositar(200)
