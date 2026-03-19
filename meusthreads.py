37cf5647ffdb5c3b583cc0f2104aee3fd0328e23
Antonio Flavio Vieira da Silva <flaviovieira94@gmail.com>
2026-03-19 11:46:29 -0300

    meustreds

--- meusthreads.py
+++ meusthreads.py
@@ -1,6 +1,6 @@
 from time import sleep
 from threading import Thread
-
+from threading import Lock
 """ class MeuThread(Thread):
     def __init__(self, texto, tempo):
         self.texto = texto
@@ -24,18 +24,28 @@ for i in range(20):
 class Ingressos:
     def __init__(self, estoque):
         self.estoque = estoque
+        self.lock = Lock()
         
     def comprar(self, quantidade):
+        self.lock.acquire()
         if self.estoque < quantidade:
             print('Não temos ingressos suficietes.')
+            self.lock.release()
             return
-            
+        sleep(1)
+        
         print(f'Você comprou {quantidade} ingressos. Ainda temos {self.estoque}')
         self.estoque -= quantidade
         
+        self.lock.release()
+        
 if __name__ == '__main__':
-    ingressos = Ingressos(30)
-    ingressos.comprar(10)
-    ingressos.comprar(30)
+    ingressos = Ingressos(10)
+   
+    for i in range(1, 20):
+        t = Thread(target=ingressos.comprar, args=(1,))
+        t.start()
+        
+    print(ingressos.estoque)
         
     
